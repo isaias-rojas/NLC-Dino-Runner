e4bfe24180777ddfc2ac1138eb6e76911191924a
Isaias Rojas <s_isaiasr@tutanota.com>
2021-09-08 19:59:06 -0400

    class 2
    initialize pygame and add background image

--- nlc_dino_runner/main.py
+++ nlc_dino_runner/main.py
@@ -1,4 +1,6 @@
-import pygame
+from nlc_dino_runner.componets.game import Game
+from nlc_dino_runner.utils import constants
 
-
-print('my commit')
+if __name__ == "__main__":
+    game = Game()
+    game.run()
